ec458c233b47a83c9ae417a6b9503f511d447384
Joaquim Francalanci <jofrancalanci@gmail.com>
2024-08-15 13:54:25 +0200

    Importazione

---
+++ EDMA.py
@@ -0,0 +1,7 @@
+#Importazioni Librerie
+import pandas as pd
+import numpy as np
+
+#Importazione Dataset
+file_path = r"cedefop-datatable.csv"
+df = pd.read_csv(file_path, encoding='ISO-8859-1', delimiter=';')
