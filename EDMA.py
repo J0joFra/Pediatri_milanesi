0491f548f327663669a55bed608575c486b3dec6
Joaquim Francalanci <jofrancalanci@gmail.com>
2024-08-15 13:55:17 +0200

    Delimiter ,

--- EDMA.py
+++ EDMA.py
@@ -4,4 +4,7 @@ import numpy as np
 
 #Importazione Dataset
 file_path = r"cedefop-datatable.csv"
-df = pd.read_csv(file_path, encoding='ISO-8859-1', delimiter=';')
+df = pd.read_csv(file_path, encoding='ISO-8859-1', delimiter=',')
+
+#Prime righe del dataset
+print(df.head())
