050564178a3844839b89671e02b7c73e36578b3d
Joaquim Francalanci <jofrancalanci@gmail.com>
2024-08-22 15:12:02 +0200

    Richiesta GET

--- Extraction_and_cleaning.py
+++ Extraction_and_cleaning.py
@@ -3,5 +3,18 @@ import urllib.request
 import json
 import pandas
 
+# Url dell'Api
+url = 'https://dati.comune.milano.it/api/3/action/datastore_search?resource_id=22b05e1f-c5d2-4468-90e5-c098977856ef&limit=5&q=title:jones'
+
+# Richiesta GET dell'API
+with urllib.request.urlopen(url) as response:
+    # Leggi la risposta e decodificala
+    data = response.read().decode()
+
+    # Converti il contenuto in JSON
+    json_data = json.loads(data)
+
+    # Stampa i dati JSON
+    print(json_data)
 
 
