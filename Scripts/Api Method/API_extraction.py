92973ecd79b747c6b8d97ca9756341385318a8fe
Joaquim Francalanci <jofrancalanci@gmail.com>
2024-10-29 17:14:18 +0100

    Address Created

--- Scripts/Api Method/API_extraction.py
+++ Scripts/Api Method/API_extraction.py
@@ -11,15 +11,15 @@ data = json.load(fileobj)
 
 records = data["result"]["records"]
 
-# Dataframe
+# DataFrame
 df = pd.DataFrame(records)
 print(f"{df}\n{df.columns}")
 
-# Capitalize
+# Capitalizza i titoli delle colonne
 df.columns = [col.capitalize() for col in df.columns]
 df = df.applymap(lambda x: x.capitalize() if isinstance(x, str) else x)
 
-# Raname Columns
+# Rinomina le colonne
 df = df.rename(columns={
     "Idmedico": "ID_med",
     "Nomemedico": "Name_med",
@@ -27,26 +27,35 @@ df = df.rename(columns={
     "Nil": "Zone",
     "Long_x_4326": "Long",
     "Lat_y_4326": "Lat",
-    "codice_regionale_medico": "Code_med",
-    'dataNascita' : "Age",
-    "Idmedico": "ID_med",
-    "Nomemedico": "Name_med",
-    "Cognomemedico": "Surname_med",
-    "ID_Nil": "ID_zone",
-    "Long_x_4326": "Long",
-    "Lat_y_4326": "Lat",
     "Codice_regionale_medico": "Code_med",
-    'dataNascita' : "Age",
+    'DataNascita': "Age",
+    "ID_Nil": "ID_zone",
 })
-
-print(f"{df}\n{df.columns}")
 
 df = df.rename(columns={
     "Tipomedico": "Type_med",
     "Attivo": "Open",
     "Ambulatorioprincipale": "Main_amb",
+    "Luogo_ambulatorio": "L_ambul",
 })
 
 print(f"{df}\n{df.columns}")
 
+# Colonna combinata 'Via', 'Civico' e 'Luogo_ambulatorio'
+def create_address(row):
+    parts = []
+    if pd.notna(row['Via']) and row['Via']:
+        parts.append(row['Via'])
+    if pd.notna(row['Civico']) and row['Civico']:
+        parts.append(row['Civico'])
+    if pd.notna(row['L_ambul']) and row['L_ambul']:
+        parts.append(row['L_ambul'])
+    return ', '.join(parts)
 
+df['Address'] = df.apply(create_address, axis=1)
+df['Address'] = df['Address'].str.title()
+
+df = df.drop(['_id','Via', 'Civico', 'L_ambul'], axis=1)
+
+print(df['Address'])
+print(df.columns)
