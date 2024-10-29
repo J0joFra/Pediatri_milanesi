e18c4bff13c9a948ed2e1d6d2f9b07a1f9c73f35
Joaquim Francalanci <jofrancalanci@gmail.com>
2024-10-29 16:44:25 +0100

    Estrazione e ristrutturazione dataset

--- Scripts/Api Method/API_extraction.py
+++ Scripts/Api Method/API_extraction.py
@@ -1,9 +1,42 @@
 import urllib.request
 import json
+import pandas as pd
 
+# URL del dataset
 url = 'https://dati.comune.milano.it/api/3/action/datastore_search?resource_id=22b05e1f-c5d2-4468-90e5-c098977856ef&limit=5'
+
+# Apertura URL e lettura dei dati JSON
 fileobj = urllib.request.urlopen(url)
 data = json.load(fileobj)
 
-# Stampa i record
-print(data["result"]["records"])
+records = data["result"]["records"]
+
+# Dataframe
+df = pd.DataFrame(records)
+print(f"{df}\n{df.columns}")
+
+# Capitalize
+df.columns = [col.capitalize() for col in df.columns]
+df = df.applymap(lambda x: x.capitalize() if isinstance(x, str) else x)
+
+# Raname Columns
+df = df.rename(columns={
+    "Idmedico": "ID_med",
+    "Nomemedico": "Name_med",
+    "Cognomemedico": "Surname_med",
+    "Nil": "Zone",
+    "Long_x_4326": "Long",
+    "Lat_y_4326": "Lat",
+    "codice_regionale_medico": "Code_med",
+    'dataNascita' : "Age",
+    "Idmedico": "ID_med",
+    "Nomemedico": "Name_med",
+    "Cognomemedico": "Surname_med",
+    "ID_Nil": "ID_zone",
+    "Long_x_4326": "Long",
+    "Lat_y_4326": "Lat",
+    "codice_regionale_medico": "Code_med",
+    'dataNascita' : "Age",
+})
+
+print(f"{df}\n{df.columns}")
