9a4cb6a5417718467a0e03fa6d4830fb7bda2b19
Joaquim Francalanci <jofrancalanci@gmail.com>
2024-10-30 22:13:30 +0100

    Searching & Extraction from Mongo

--- Flask/app.py
+++ Flask/app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+# Connessione a MongoDB
+client = MongoClient("mongodb+srv://jofrancalanci:Cf8m2xsQdZgll1hz@element.2o7dxct.mongodb.net/")
+db = client['Healthcare']
+collection = db['Pediatri']
+
 @app.route('/')
 def home():
-    return render_template('index.html') 
+    query = request.args.get('search')
+    
+    if query:
+        # Cerca nei campi nome, zona, e specializzazione
+        pediatri = collection.find({
+            "$or": [
+                {"Name_med": {"$regex": query, "$options": "i"}},
+                {"Surname_med": {"$regex": query, "$options": "i"}},
+                {"Zone": {"$regex": query, "$options": "i"}}
+            ]
+        })
+    else:
+        pediatri = collection.find()
+
+    return render_template('index.html', pediatri=pediatri)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
